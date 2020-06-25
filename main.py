7ddac6dd807f219e0fd2f725819bdc0b66d25b15
Ryan Howard <ryan.howard@howqa.co.uk>
2020-06-25 14:47:08 +0100

    env variabes linting issues

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from api import run_job, check_job_status
+import os
 
-#project_id = 'jrKiet1cHEyKBTcZMmLoMg'
-#job_id = 'heb20uUMlkSeTvaM8K8fFg'
-#auth = 'wiaj2NlksX-SGf6VJctehppP1PLlYNZ5q4FNiQVY7wY1'
+# project_id = 'jrKiet1cHEyKBTcZMmLoMg'
+# job_id = 'heb20uUMlkSeTvaM8K8fFg'
+# auth = 'wiaj2NlksX-SGf6VJctehppP1PLlYNZ5q4FNiQVY7wY1'
 
-#Call API to start TestProject job
+project_id = os.environ['project_id']
+job_id = os.environ['job_id']
+auth = os.environ['authorisation_token']
+
+# Call API to start TestProject job
 start_job = run_job(project_id, job_id, auth)
 
 print(start_job[1])
