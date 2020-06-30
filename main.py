79d0a140f09875218e51ef7d469017759de2cdf4
Ryan Howard <ryan.howard@howqa.co.uk>
2020-06-30 20:37:36 +0100

    fixed linting errors

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 
 os.environ["INPUT_PROJECT_ID"] = 'jrKiet1cHEyKBTcZMmLoMg'
 os.environ["INPUT_JOB_ID"] = 'heb20uUMlkSeTvaM8K8fFg'
-os.environ["INPUT_AUTHORISATION_TOKEN"] = 'wiaj2NlksX-SGf6VJctehppP1PLlYNZ5q4FNiQVY7wY1'
+os.environ["INPUT_AUTHORISATION_TOKEN"] = \
+    'wiaj2NlksX-SGf6VJctehppP1PLlYNZ5q4FNiQVY7wY1'
 
 project_id = os.environ['INPUT_PROJECT_ID']
 job_id = os.environ['INPUT_JOB_ID']
@@ -18,37 +19,38 @@ if response_code == 200:
     check_job = check_job_status(project_id, job_id, start_job[0], auth)
     check_response_code = check_job[1]
     if check_response_code == 200:
-        print('The current job status is {check_job}'.format(check_job=check_job[0]))
+        print('The current job status is {check_job}'
+              .format(check_job=check_job[0]))
     elif check_response_code == 412:
-        print("Job failed to start, status code returned was {}. Check that your agent is running"
-              .format(response_code))
+        print("Job failed to start, status code returned was {}. "
+              "Check that your agent is running".format(response_code))
         exit(1)
     elif check_response_code == 401:
-        print("Job failed to run, status code returned was {}. Check The Project Id and Authorisation code are correct"
-              .format(response_code))
+        print("Job failed to run, status code returned was {}. "
+              "Check The Project Id and Authorisation code are correct".format(response_code))
         exit(1)
     elif check_response_code == 400:
-        print("Job failed to run, status code returned was {}. Check The Job Id is correct"
-              .format(response_code))
+        print("Job failed to run, status code returned was {}. "
+              "Check The Job Id is correct".format(response_code))
         exit(1)
     else:
-        print("Error running job, status code returned was {}.".format(response_code))
+        print("Error running job, status code returned was {}."
+              .format(response_code))
         exit(1)
 
 elif response_code == 412:
-    print("Job failed to start, status code returned was {}. Check that your agent is running"
-          .format(response_code))
+    print("Job failed to start, status code returned was {}. "
+          "Check that your agent is running".format(response_code))
     exit(1)
 elif response_code == 401:
-    print("Job failed to run, status code returned was {}. Check The Project Id and Authorisation code are correct"
-          .format(response_code))
+    print("Job failed to run, status code returned was {}. "
+          "Check The Project Id and Authorisation code are correct".format(response_code))
     exit(1)
 elif response_code == 400:
-    print("Job failed to run, status code returned was {}. Check The Job Id is correct"
-          .format(response_code))
+    print("Job failed to run, status code returned was {}. "
+          "Check The Job Id is correct".format(response_code))
     exit(1)
 else:
-    print("Error running job, status code returned was {}.".format(response_code))
+    print("Error running job, status code returned was {}."
+          .format(response_code))
     exit(1)
-
-
